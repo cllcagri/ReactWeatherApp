672d6e2593efa5f983999b34cbcec75ba06f40e5
celal <c.cagriyilmaz@gmail.com>
2019-11-15 22:31:39 +0300

    Weather App connection and route configuration

---
+++ react-weather-app/server/models/weather.js
@@ -0,0 +1,20 @@
+const request = require('request-promise');
+
+const API_KEY = '224f2b398d2f661c648802d6b12f49ab';
+
+class Weather {
+    static retrieveByCity(city, callback){
+        let fullUri = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + API_KEY;
+        request({
+            uri: fullUri,
+            json: true
+        }).then(function (res) {
+           callback(res);
+        }).catch(function (err) {
+            console.log(err);
+            callback({error: err});
+        });
+    }
+}
+
+module.exports = Weather;
